65382a73ad94f2e3a5de7caa61050c594d807690
mewadamithlesh2213 <mewadamithlesh@gmail.com>
2026-02-03 12:08:38 +0530

    This File is The Example Of Empty Project

---
+++ Empty_Project_Example/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Empty_Project_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
